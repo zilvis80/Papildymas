908f514814fe415f58b5e711336a1def345bd794
zilvinas <zilvis80@mail.ru>
2024-02-20 22:54:54 +0200

    najinimas

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.scss';
 
 function Navbar() {
+  const [user, setUser] = useState('loading....');
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users/1'
+        );
+        if (!response.ok) throw new Error('Somteng wrong');
+        const data = await response.json();
+        console.log(data);
+        setUser(data.username);
+      } catch (error) {}
+    };
+    fetchUser();
+  }, []);
+
   return (
     <nav className='nav-container'>
       <h1>My Shop</h1>
@@ -17,6 +33,7 @@ function Navbar() {
           <NavLink to='/Favorite'>Favorite</NavLink>
         </li>
       </ul>
+      <h1>{user}</h1>
     </nav>
   );
 }
